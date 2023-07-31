eb06c037487f01ae6bcd2e205d8a6d90cebdca17
Malik Elbadri <MalikCura@users.noreply.github.com>
2023-08-01 00:02:22 +0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+(self["webpackChunktext_cam"] = self["webpackChunktext_cam"] || []).push([["main"],{
+
+/***/ "./src/scripts/index.ts":
+/*!******************************!*\
+  !*** ./src/scripts/index.ts ***!
+  \******************************/
+/***/ (() => {
+
+console.log("HELLO");
+const x = 10;
+for (let i = 0; i < 100; i++) {
+  console.log(x);
+}
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/scripts/index.ts"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBO0FBQ0E7QUFDQSxnQkFBZ0IsU0FBUztBQUN6QjtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdGV4dC1jYW0vLi9zcmMvc2NyaXB0cy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkhFTExPXCIpO1xyXG5jb25zdCB4ID0gMTA7XHJcbmZvciAobGV0IGkgPSAwOyBpIDwgMTAwOyBpKyspIHtcclxuICBjb25zb2xlLmxvZyh4KTtcclxufVxyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
