0c0db88126641952a00a5c1e8deadc5a96379355
Malik Elbadri <MalikCura@users.noreply.github.com>
2023-08-01 23:37:54 +0300

    Updated characters

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,72 +1,56 @@
 (async () => {
-  const video = document.getElementById("video") as HTMLVideoElement;
+  const video = document.createElement("video");
   const canvas = document.createElement("canvas");
-  const text = document.getElementById("p") as HTMLParagraphElement;
-
-  document.body.append(canvas);
-
+  const text = document.getElementById("p") as HTMLPreElement;
   const context = canvas.getContext("2d");
+
+  const characters = `$@B%8&WM#*_+~i!lI;:,'.`.split("");
+  characters.push("&nbsp;");
 
   const stream = await navigator.mediaDevices.getUserMedia({
     video: true,
   });
+
   video.srcObject = stream;
 
   let string = "";
 
-  video.onplay = (e) => {
-    const width = video.videoWidth / 2;
-    const height = video.videoHeight / 2;
+  video.onplay = () => {
+    const width = 120;
+    const height = 60;
 
-    const timerCallback = () => {
+    const timer = () => {
       context.drawImage(video, 0, 0, width, height);
+      const frame = context.getImageData(0, 0, width, height);
 
-      const frame = context.getImageData(0, 0, width, height, {
-        colorSpace: "display-p3",
-      });
       canvas.height = height;
       canvas.width = width;
       const data = frame.data;
       string = "";
 
       for (let i = 0, j = 0; i < data.length; i += 4, j++) {
-        const red = data[i + 0];
-        const green = data[i + 1];
-        const blue = data[i + 2];
-        const gray = (red + green + blue) / 3;
-        data[i + 0] = gray;
-        data[i + 1] = gray;
-        data[i + 2] = gray;
+        const r = data[i + 0];
+        const g = data[i + 1];
+        const b = data[i + 2];
+        const gray = (r + g + b) / 3;
+        const gPercent = gray / 255;
+        const index = Math.floor(characters.length * gPercent);
+        string += characters[index] || ".";
 
-        let char = "_";
-        if (gray > 80) {
-          char = ".";
-        }
-        if (gray > 100) {
-          char = "*";
-        }
-        if (gray > 150) {
-          char = "$";
-        }
-        if (gray > 200) {
-          char = "@";
-        }
-
-        string += char;
-        if (j % 320 === 1) string += "<br/>";
+        if (j % width === 1) string += "<br/>";
       }
+
       text.innerHTML = string;
+
       context.putImageData(frame, 0, 0);
 
-      setTimeout(() => {
-        timerCallback();
-      });
+      setTimeout(() => timer());
     };
-    timerCallback();
+    timer();
   };
   video.controls = true;
 
   setTimeout(() => {
     video.play();
-  }, 400);
+  }, 1000);
 })();
