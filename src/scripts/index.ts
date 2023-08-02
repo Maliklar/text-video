d562bf6ec4cde26002ef1fa2c06b8da8d4851ebc
Malik Elbadri <mbadri@cura.healthcare>
2023-08-02 11:01:42 +0300

    Added comments

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,56 +1,77 @@
-(async () => {
-  const video = document.createElement("video");
-  const canvas = document.createElement("canvas");
-  const text = document.getElementById("p") as HTMLPreElement;
-  const context = canvas.getContext("2d");
+// Creating needed HTML elements
 
-  const characters = `$@B%8&WM#*_+~i!lI;:,'.`.split("");
-  characters.push("&nbsp;");
+const video = document.createElement("video");
+const canvas = document.createElement("canvas");
+const text = document.getElementById("pre") as HTMLPreElement;
+const playButton = document.getElementById("playButton") as HTMLButtonElement;
+// Initialize canvas context
+const context = canvas.getContext("2d");
 
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-  });
+// Characters ordered based on their brightness (This one may need to change)
+const characters = `$@B%8&WM#*_+~i!lI;:,'.`.split("");
+characters.push("&nbsp;"); // Space entity
 
-  video.srcObject = stream;
+let videoString = "";
 
-  let string = "";
+// Handle when video is playing
+video.onplay = () => {
+  // Setting video resolution
+  const width = 120;
+  const height = 60;
 
-  video.onplay = () => {
-    const width = 120;
-    const height = 60;
+  const frameCapture = () => {
+    // Take the current frame on the video and draw it as an image on the canvas
+    context.drawImage(video, 0, 0, width, height);
 
-    const timer = () => {
-      context.drawImage(video, 0, 0, width, height);
-      const frame = context.getImageData(0, 0, width, height);
+    // Extract the frame image from canvas as bit representation Uint8ClampedArray
+    const frame = context.getImageData(0, 0, width, height);
 
-      canvas.height = height;
-      canvas.width = width;
-      const data = frame.data;
-      string = "";
+    const pixels = frame.data;
+    // Empty video string from previous frame data
+    videoString = "";
 
-      for (let i = 0, j = 0; i < data.length; i += 4, j++) {
-        const r = data[i + 0];
-        const g = data[i + 1];
-        const b = data[i + 2];
-        const gray = (r + g + b) / 3;
-        const gPercent = gray / 255;
-        const index = Math.floor(characters.length * gPercent);
-        string += characters[index] || ".";
+    // The data will be: [r, g, b, a, r, g, b, a, ...] pixels
+    for (let i = 0, j = 0; i < pixels.length; i += 4, j++) {
+      // Extract Red, Green, and Blue from each pixel (Ignore the alpha)
+      const [r, g, b] = [pixels[i], pixels[i + 1], pixels[i + 2]];
 
-        if (j % width === 1) string += "<br/>";
-      }
+      // Convert the RGB to Grayscale
+      const gray = (r + g + b) / 3;
 
-      text.innerHTML = string;
+      // get the brightness from the grayscale as a percentage
+      const gPercent = gray / 255;
 
-      context.putImageData(frame, 0, 0);
+      // Get the character based on brightness
+      const index = Math.floor((characters.length - 1) * gPercent);
+      videoString += characters[index] || ".";
 
-      setTimeout(() => timer());
-    };
-    timer();
+      if (j % width === 1) videoString += "<br/>";
+    }
+
+    text.innerHTML = videoString;
+
+    setTimeout(() => {
+      frameCapture();
+    });
   };
-  video.controls = true;
 
+  // Call frame capture recursively based on the event queue (timeout)
+  frameCapture();
+};
+
+video.onpause = () => {};
+
+// User clicks on play button
+async function play() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+    video.srcObject = stream;
+  } catch {
+    alert("Camera permissions are needed");
+  }
   setTimeout(() => {
     video.play();
   }, 1000);
-})();
+}
